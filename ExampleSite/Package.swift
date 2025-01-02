4d662c999b1e649d755390b56b3449a2d016ec7f
sungjun-hong-ikoob <74225754+hogumachu@users.noreply.github.com>
2025-01-02 14:44:40 +0900

    renamed package information

--- ExampleSite/Package.swift
+++ ExampleSite/Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "IgniteStarter",
-    platforms: [.macOS(.v13)],
-    dependencies: [
-        .package(url: "https://github.com/twostraws/Ignite.git", branch: "main")
-    ],
-    targets: [
-        .executableTarget(
-            name: "IgniteStarter",
-            dependencies: ["Ignite"]),
-    ]
+  name: "ExampleStie",
+  platforms: [.macOS(.v13)],
+  dependencies: [
+    .package(url: "https://github.com/twostraws/Ignite.git", branch: "main")
+  ],
+  targets: [
+    .executableTarget(
+      name: "ExampleStie",
+      dependencies: ["Ignite"]),
+  ]
 )
